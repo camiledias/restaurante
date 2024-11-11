44cc0d2bd95576d8a0532985ca3982bfa40e0af7
camiledias <camilesdsilva@gmail.com>
2024-11-11 14:41:42 -0300

    Aaula gustavo

---
+++ restuarante.java
@@ -0,0 +1,30 @@
+import java.until.Scanner;
+import java.applet.Array;
+
+public pratos{
+    String nome;
+    String descrição;
+    String preço;
+    String satus;
+
+}
+
+
+public class restuarante{
+
+
+
+
+
+}
+
+public static void main(String[] args){
+    Scanner scan = new Scanne(System.in);
+    ArrayList<Prato> pratos = new ArrayList<>();
+
+
+
+
+
+
+}
